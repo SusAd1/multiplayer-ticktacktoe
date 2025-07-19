d1c29d8b1015fbec2903324b6d1bd95881cf9230
SusAd1 <kkali3891@gmail.com>
2025-07-19 08:25:17 +0545

    Add move and checkDraw methods to GameManager

--- static/gameManager.js
+++ static/gameManager.js
@@ -16,6 +16,24 @@ class GameManager{
             this.cells.push(cell)
         }
     }
+    move(player,cellNo,restartGame = true){
+        const cell = this.cells[cellNo]
+        if (player != this.currentPlayer || cell.textContent != '') return;
+
+        cell.textContent = this.currentPlayer;
+
+        if (this.checkWin()){
+            if(restartGame) 
+                if (confirm(`${this.currentPlayer} wins!. do you want to play again`)) this.restartGame();
+        }
+        else if (this.checkDraw()) {
+            if(restartGame) 
+                if (confirm(`its a draw. do you want to play again`))  this.restartGame();
+        }
+        else {
+            this.currentPlayer = (this.currentPlayer == 'X' )? 'O' : 'X';
+        }
+    }
     checkWin(){
         const winPatterns = [
             [0,1,2], [3,4,5], [6,7,8],
@@ -25,6 +43,9 @@ class GameManager{
         return winPatterns.some(pattern =>
             pattern.every(index => this.cells[index].textContent === this.currentPlayer)
         );
+    }
+    checkDraw(){
+        return cells.every(c => c.textContent != '')
     }
     restartGame() {
         this.currentPlayer = 'X';
